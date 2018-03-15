798e930d007198f7a1b1f9a99f81d300269af81a
Diogo Ferreira <pdiogoferreira23@gmail.com>
2018-03-15 19:33:42 +0000

    Ended general repository

--- src/generalRepository/GeneralRepository.java
+++ src/generalRepository/GeneralRepository.java
@@ -5,12 +5,21 @@ import states.BrokerState;
 import states.SpectatorState;
 import states.State;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GeneralRepository {
 
-    private static PrintWriter pw;
+    private PrintWriter pw;
+    private File file;
+
+    private Lock mutex;
 
     private State brokerState;
     private State[] spectatorsState;
@@ -18,7 +27,6 @@ public class GeneralRepository {
     private int raceNumber;
     private State[] horsesState;
     private int[] horsesAgility;
-    private int trackLength;
     private int[] spectatorsBettedHorse;
     private double[] spectatorsBet;
     private double[] horsesOdd;
@@ -27,13 +35,13 @@ public class GeneralRepository {
     private boolean[] horsesEnded;
 
     public GeneralRepository() {
+        this.mutex = new ReentrantLock();
         this.brokerState = BrokerState.OPENING_THE_EVENT;
         this.spectatorsState = new State[EventVariables.NUMBER_OF_SPECTATORS];
         this.spectatorsWallet = new double[EventVariables.NUMBER_OF_SPECTATORS];
         this.raceNumber = 0;
         this.horsesState = new State[EventVariables.NUMBER_OF_HORSES_PER_RACE];
         this.horsesAgility = new int[EventVariables.NUMBER_OF_HORSES_PER_RACE];
-        this.trackLength = EventVariables.RACING_TRACK_LENGTH;
         this.spectatorsBettedHorse = new int[EventVariables.NUMBER_OF_SPECTATORS];
         this.spectatorsBet = new double[EventVariables.NUMBER_OF_SPECTATORS];
         this.horsesOdd = new double[EventVariables.NUMBER_OF_HORSES_PER_RACE];
@@ -48,65 +56,169 @@ public class GeneralRepository {
         // Initialize spectators wallet
         for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
             spectatorsWallet[i] = EventVariables.INITIAL_WALLET;
+
+        // Log file settings
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat date = new SimpleDateFormat("yyyyMMddhhmmss");
+        String filename = "AfternoonAtRaces_" + date.format(today) + ".log";
+
+        this.file = new File(filename);
+
+        printHeader();
     }
 
     public void setBrokerState(State brokerState) {
+        mutex.lock();
+
         this.brokerState = brokerState;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setSpectatorState(int spectatorId, State spectatorState) {
+        mutex.lock();
+
         this.spectatorsState[spectatorId] = spectatorState;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setSpectatorWallet(int spectatorId, double spectatorWallet) {
+        mutex.lock();
+
         this.spectatorsWallet[spectatorId] = spectatorWallet;
+
+        mutex.unlock();
     }
 
     public void setRaceNumber(int raceNumber) {
+        mutex.lock();
+
         this.raceNumber = raceNumber;
+
+        mutex.unlock();
     }
 
     public void setHorseState(int horseIdx, State horseState) {
+        mutex.lock();
+
         this.horsesState[horseIdx] = horseState;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setHorseAgility(int horseIdx, int horseAgility) {
+        mutex.lock();
+
         this.horsesAgility[horseIdx] = horseAgility;
+
+        mutex.unlock();
     }
 
     public void setSpectatorsBettedHorse(int spectatorId, int spectatorBettedHorse) {
+        mutex.lock();
+
         this.spectatorsBettedHorse[spectatorId] = spectatorBettedHorse;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setSpectatorsBet(int spectatorId, double spectatorBet) {
+        mutex.lock();
+
         this.spectatorsBet[spectatorId] = spectatorBet;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setHorseOdd(int horseIdx, double horseOdd) {
+        mutex.lock();
+
         this.horsesOdd[horseIdx] = horseOdd;
+
+        mutex.unlock();
     }
 
     public void setHorseStep(int horseIdx, int horseStep) {
+        mutex.lock();
+
         this.horsesStep[horseIdx] = horseStep;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setHorsePosition(int horseIdx, int horsePosition) {
+        mutex.lock();
+
         this.horsesPosition[horseIdx] = horsePosition;
+        printState();
+
+        mutex.unlock();
     }
 
     public void setHorseEnded(int horseIdx, boolean horseEnded) {
+        mutex.lock();
+
         this.horsesEnded[horseIdx] = horseEnded;
+        printState();
+
+        mutex.unlock();
     }
 
-    public void initialLog() {
-        pw = new PrintWriter();
-        pw.printf("AFTERNOON AT THE RACE TRACK - Description of the internal state of the problem\n\n");
-        pw.printf("MAN/BRK SPECTATOR/BETTER HORSE/JOCKEY PAIR at Race RN\n");
-        pw.printf(" Stat St0 Am0 St1 Am1 St2 Am2 St3 Am3 RN St0 Len0 St1 Len1 St2 Len2 St3 Len3\n");
-        pw.printf(" Race RN Status\n");
-        pw.printf(" RN Dist BS0 BA0 BS1 BA1 BS2 BA2 BS3 BA3 Od0 N0 Ps0 SD0 Od1 N1 Ps1 Sd1 Od2 N2 Ps2 Sd2 Od3 N3 Ps3 St3\n");
+    private void printHeader() {
+        mutex.lock();
 
+        try {
+            pw = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        pw.println("AFTERNOON AT THE RACE TRACK - Description of the internal state of the problem\n");
+        pw.printf("MAN/BRK SPECTATOR/BETTER HORSE/JOCKEY PAIR at Race %d\n", raceNumber);
+        pw.print(" Stat");
         for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
-            pw.printf("%4s %3s", spectatorsState[i], spectatorsWallet[i]);
+            pw.printf(" St%d Am%d ", i, i);
+        pw.print("RN");
+        for (int i = 0; i < EventVariables.NUMBER_OF_HORSES_PER_RACE; i++)
+            pw.printf(" St%d Len%d ", i, i);
+        pw.println();
+        pw.printf("\t\t\tRace %d Status\n", raceNumber);
+        pw.print(" RN Dist");
+        for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
+            pw.printf(" BS%d BA%d ", i, i);
+        for (int i = 0; i < EventVariables.NUMBER_OF_HORSES_PER_RACE; i++)
+            pw.printf(" Od%d N%d Ps%d SD%s ", i, i, i, i);
+        pw.println();
+        printState();
+
+        mutex.unlock();
+    }
+
+    private void printState() {
+        mutex.lock();
+
+        pw.printf("  %4s", brokerState);
+        for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
+            pw.printf(" %3s %4f", spectatorsState[i], spectatorsWallet[i]);
+        pw.printf("%1d", raceNumber);
+        for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
+            pw.printf(" %3s %2d", horsesState[i], horsesAgility[i]);
+        pw.println();
+        pw.printf(" %1d %2d", raceNumber, EventVariables.RACING_TRACK_LENGTH);
+        for (int i = 0; i < EventVariables.NUMBER_OF_SPECTATORS; i++)
+            pw.printf(" %1d %4f", spectatorsBettedHorse[i], spectatorsBet[i]);
+        for (int i = 0; i < EventVariables.NUMBER_OF_HORSES_PER_RACE; i++)
+            pw.printf(" %4f %2d %2d %1s", horsesOdd[i], horsesStep[i],
+                    horsesPosition[i], horsesEnded[i]);
+        pw.println();
+
+        mutex.unlock();
     }
 }
